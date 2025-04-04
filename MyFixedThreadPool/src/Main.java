1d64932574dc80a3a26442002f530b35d77a0d1b
li-fafu <2904144774@qq.com>
2025-04-05 00:54:51 +0800

    自行实现一个简单的线程池. 只实现一个 "固定线程数目" 的线程池

--- MyFixedThreadPool/src/Main.java
+++ MyFixedThreadPool/src/Main.java
@@ -1,7 +1,39 @@
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 class MyFixedThreadPool {
+    private BlockingQueue<Runnable> queue = new LinkedBlockingQueue<Runnable>();
 
+    public MyFixedThreadPool(int n) {
+        for (int i = 0; i < n; i++) {
+            //创建线程
+            Thread t = new Thread(() -> {
+                //每个线程要完成的工作：从任务队列取出任务然后执行
+                try{
+                    while(true){
+                        Runnable task = queue.take();
+                        task.run();
+                    }
+                } catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+            });
+            t.start();
+        }
+    }
+    public void submit(Runnable task) throws InterruptedException {
+        queue.put(task);
+    }
 }
 
 public class Main {
+    public static void main (String[] args) throws InterruptedException {
+        MyFixedThreadPool threadPool = new MyFixedThreadPool(4);
+        for (int i = 0; i < 100; i++){
+            int id = i;
+            threadPool.submit(()->{
+                System.out.println("执行任务：" + id);
+            });
+        }
+    }
 }
