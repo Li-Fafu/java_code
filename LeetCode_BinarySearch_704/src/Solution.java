57737fc36206ce47cc16434b52c1ac07b7fc25d2
li-fafu <2904144774@qq.com>
2025-04-22 10:54:25 +0800

    最基础二分查找

--- LeetCode_BinarySearch_704/src/Solution.java
+++ LeetCode_BinarySearch_704/src/Solution.java
@@ -4,7 +4,9 @@ class Solution {
         int left = 0;
         int right = nums.length - 1;
         while(left <= right){
-            int mid = (right + left) / 2;
+            //int mid = (right + left) / 2;
+            //防止出现溢出
+            int mid = left + (right - left) / 2;
             if(nums[mid] == target){
                 return mid;
             }else if(nums[mid] < target){
