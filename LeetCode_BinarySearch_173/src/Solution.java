022b6154ba8e73caf67c60fa8adb9fbb7fda0fcb
li-fafu <2904144774@qq.com>
2025-04-24 14:36:18 +0800

    点名

--- LeetCode_BinarySearch_173/src/Solution.java
+++ LeetCode_BinarySearch_173/src/Solution.java
@@ -2,6 +2,16 @@
  * 某班级 n 位同学的学号为 0 ~ n-1。点名结果记录于升序数组 records。
  * 假定仅有一位同学缺席，请返回他的学号。
  */
+
+/**
+ * 解法：
+ * 1、哈希
+ * 2、直接遍历；
+ * 3、位运算
+ * 4、数学（高斯求和）
+ */
+
+//5、二分查找
 class Solution {
     public int takeAttendance(int[] records) {
         int left = 0;
