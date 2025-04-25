e4fb9f117da0ce57cfa893084b7d2eb3935b5815
li-fafu <2904144774@qq.com>
2025-04-25 15:18:00 +0800

    除自身以外数组的乘积

---
+++ LeetCode_PrefixProduct_238/src/Solution2.java
@@ -0,0 +1,16 @@
+public class Solution2 {
+    public int[] productExceptSelf(int[] nums) {
+        int n=nums.length;
+        int[] left=new int[n];
+        left[0]=1;
+        for(int i=1;i<n;i++){
+            left[i]=nums[i-1]*left[i-1];
+        }
+        int R=1;
+        for(int i=n-1;i>=0;i--){
+            left[i]*=R;
+            R*=nums[i];
+        }
+        return left;
+    }
+}
